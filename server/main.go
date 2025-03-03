31888fb26766aec30d230f42676d5c5b0465d577
as283 <as283@alu.ua.es>
2025-03-03 17:38:43 +0100

    Añadido flags para cert y host en server

--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/tls"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,31 +11,35 @@ import (
 	"github.com/quic-go/quic-go/http3"
 )
 
-const (
-	addr = "0.0.0.0:4433"
-	cert = "../varuna.cauth.crt"
-	key  = "../varuna.cauth.key"
+var (
+	addr *string
+	cert *string
+	key  *string
 )
 
 func main() {
+	addr = flag.String("ip", "0.0.0.0:4433", "Host IP and port")
+	cert = flag.String("cert", "localhost.pem", "TLS Certificate")
+	key = flag.String("key", "localhost-key.pem", "TLS Key")
+
 	router := http.NewServeMux()
 
 	router.Handle("GET /hello", http.HandlerFunc(handler.HandleHello))
 
-	fmt.Println("Server started on https://" + addr)
+	fmt.Println("Server started on https://" + *addr)
 
 	if useHttp3 := true; useHttp3 {
 		server := &http3.Server{
-			Addr:      addr,
+			Addr:      *addr,
 			Handler:   router,
 			TLSConfig: TLSConfig(),
 		}
 
-		if err := server.ListenAndServeTLS(cert, key); err != nil {
+		if err := server.ListenAndServeTLS(*cert, *key); err != nil {
 			panic(err)
 		}
 	} else {
-		if err := http.ListenAndServeTLS(addr, cert, key, router); err != nil {
+		if err := http.ListenAndServeTLS(*addr, *cert, *key, router); err != nil {
 			panic(err)
 		}
 	}
@@ -42,7 +47,7 @@ func main() {
 }
 
 func TLSConfig() *tls.Config {
-	cert, err := tls.LoadX509KeyPair(cert, key)
+	cert, err := tls.LoadX509KeyPair(*cert, *key)
 	if err != nil {
 		log.Fatal(err)
 	}
